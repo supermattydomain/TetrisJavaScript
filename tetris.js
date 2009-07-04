1adfc31561c9228d2856bb83953f73dd27ec4e44
Matt <matt@supermatty.com>
2009-07-04 05:48:53 +0000

    Add Shape class and create a test one successfully.
    git-svn-id: https://svn/svn/TetrisJavaScript@1770 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- tetris.js
+++ tetris.js
@@ -4,36 +4,107 @@ function TetrisBoard(width, height, div) {
 	this.width = width;
 	this.height = height;
 	this.div = div;
-	this.createGrid(width, height, div);
+	this.createGrid();
 	this.debugLog("TetrisBoard end init");
 }
 
 Object.extend(TetrisBoard.prototype, {
-	createGrid : function(width, height, div) {
-		var table = dce('table');
-		setClass(table, 'board');
-		div.appendChild(table);
-		for ( var row = 0; row < height; row++) {
-			var rowElt = table.insertRow(-1);
+	createGrid : function() {
+		this.table = dce('table');
+		setClass(this.table, 'board');
+		this.div.appendChild(this.table);
+		for ( var row = 0; row < this.height; row++) {
+			var rowElt = this.table.insertRow(-1);
 			setClass(rowElt, 'board');
-			for ( var col = 0; col < width; col++) {
+			for ( var col = 0; col < this.width; col++) {
 				var cellElt = rowElt.insertCell(-1);
 				setClass(cellElt, 'empty');
 				// cellElt.appendChild(dctn(' '));
+			}
+		}
+	},
+	getTable: function() {
+		return this.table;
 	}
-}
-}
 });
 
-function Shape(row, col, type, rot) {
+var shapeBitmaps = [
+[
+ '    ',
+ ' XX ',
+ ' XX ',
+ '    '
+],
+
+[
+ '  X ',
+ '  X ',
+ '  X ',
+ '  X ',
+],
+
+[
+ '  X ',
+ ' XX ',
+ ' X  ',
+ '    ',
+],
+
+[
+ ' X  ',
+ ' XX ',
+ '  X ',
+ '    ',
+],
+
+[
+ ' X  ',
+ ' XX ',
+ ' X  ',
+ '    ',
+],
+
+[
+ ' X  ',
+ ' X  ',
+ ' XX ',
+ '    ',
+],
+
+[
+ '  X ',
+ '  X ',
+ ' XX ',
+ '    ',
+]
+
+];
+
+function Shape(table, row, col, type, rot) {
+	this.setDebug(true);
+	this.table = table;
 	this.row = row;
 	this.col = col;
 	this.type = type;
 	this.rot = rot;
+	this.bitmap = shapeBitmaps[this.type];
+	this.show();
 }
 
 Object.extend(Shape.prototype, {
+	hide: function() {
+	},
+	show: function() {
+		this.debugLog(this.bitmap);
+		for (var r = 0; r < this.bitmap.length; r++) {
+			for (var c = 0; c < this.bitmap[0].length; c++) {
+				var square = this.table.rows[this.row + r].cells[this.col + c];
+				setClass(square, this.bitmap[r][c] != ' ' ? 'shape' : 'empty');
+			}
+		}
+	},
 	rotate : function(clockwise) {
+		this.hide();
 		if (clockwise) {
 			if (++rot > 3) {
 				rot = 0;
@@ -43,5 +114,6 @@ Object.extend(Shape.prototype, {
 				rot = 3;
 			}
 		}
+		this.show();
 	}
 });
