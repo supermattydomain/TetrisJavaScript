8c456b6e60e8852d776e3beda4c77f949d46b2ab
Matt <matt@supermatty.com>
2009-07-05 03:19:05 +0000

    Add support for zapping filled rows. Make board bigger. Improve board's encapsulation of its table.
    git-svn-id: https://svn/svn/TetrisJavaScript@1780 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- tetris.js
+++ tetris.js
@@ -25,68 +25,80 @@ TetrisBoard.prototype = {
 			}
 		}
 	},
-	getTable: function() {
-		return this.table;
+	getWidth: function() {
+		return this.table.rows[0].cells.length;
+	},
+	getHeight: function() {
+		return this.table.rows.length;
+	},
+	isEmptyCell: function(cell) {
+		return cell.className == 'empty';
+	},
+	isEmpty: function(row, col) {
+		return this.isEmptyCell(this.table.rows[row].cells[col]);
+	},
+	setEmpty: function(row, col) {
+		setClass(this.table.rows[row].cells[col], 'empty');
+	},
+	setShape: function(row, col) {
+		setClass(this.table.rows[row].cells[col], 'shape');
 	},
 	createShape: function() {
-		var type = Math.round(Math.random() * (shapeBitmaps.length - 1));
-		this.currentShape = new Shape(this.table, 0, 0, type);
+		var type = Math.round(Math.random() * shapeBitmaps.length - 0.5);
+		// FIXME: Is Math.random strictly *between* 0 and 1?
+		// FIXME: Does Math.round round halves up or down or towards zero or what?
+		if (type < 0) {
+			type = 0;
+		}
+		if (type >= shapeBitmaps.length) {
+			type = shapeBitmaps.length - 1;
+		}
+		type = 0; // FIXME: For testing filled rows detection
+		this.currentShape = new Shape(this, 0, this.getWidth() / 2 - shapeBitmaps[type][0].length / 2, type);
 		this.currentShape.show();
+	},
+	isRowFilled: function(row) {
+		for (var col = 0; col < this.getWidth(); col++) {
+			if (this.isEmpty(row, col)) {
+				return false; // gap in this row
+			}
+		}
+		return true;
+	},
+	clearRow: function(row) {
+		for (var col = 0; col < this.getWidth(); col++) {
+			this.setEmpty(row, col);
+		}
+	},
+	copyRow: function(srcRow, dstRow) {
+		for (var col = 0; col < this.getWidth(); col++) {
+			var srcCell = this.table.rows[srcRow].cells[col];
+			var dstCell = this.table.rows[dstRow].cells[col];
+			setClass(dstCell, srcCell.className);
+		}
+	},
+	zapRow: function(row) {
+		// this.debugLog('Zap row ' + row);
+		for (/* NOP */; row > 0; row--) {
+			this.copyRow(row - 1, row);
+		}
+		this.clearRow(0);
+	},
+	zapFilledRows: function() {
+		// this.debugLog('zapFilledRows');
+		for (var row = this.getHeight() - 1; row >= 0; /* NOP */) {
+			if (this.isRowFilled(row)) {
+				this.zapRow(row);
+			} else {
+				row--;
+			}
+		}
 	}
 };
 
-shapeBitmaps[0] =
-[
- 'XX',
- 'XX',
- ];
-
-shapeBitmaps[1] =
-[
- '  X  ',
- '  X  ',
- '  X  ',
- '  X  '
-];
-
-shapeBitmaps[2] =
-[
- ' X',
- 'XX',
- 'X '
-];
-
-shapeBitmaps[3] =
-[
- 'X ',
- 'XX',
- ' X'
-];
-
-shapeBitmaps[4] =
-[
- ' X ',
- ' XX',
- ' X '
-];
-
-shapeBitmaps[5] =
-[
- ' X ',
- ' X ',
- ' XX'
-];
-
-shapeBitmaps[6] =
-[
- ' X ',
- ' X ',
- 'XX '
-];
-
-function Shape(table, row, col, type) {
-	this.setDebug(true);
-	this.table = table;
+function Shape(board, row, col, type) {
+	// this.setDebug(true);
+	this.board = board;
 	this.row = row;
 	this.col = col;
 	this.type = type;
@@ -101,8 +113,7 @@ Shape.prototype = {
 		for ( var r = 0; r < this.bitmap.length; r++) {
 			for ( var c = 0; c < this.bitmap[0].length; c++) {
 				if (this.bitmap[r][c] != this.empty) {
-					var square = this.table.rows[this.row + r].cells[this.col + c];
-					setClass(square, 'empty');
+					this.board.setEmpty(this.row + r, this.col + c);
 				}
 			}
 		}
@@ -112,8 +123,7 @@ Shape.prototype = {
 		for (var r = 0; r < this.bitmap.length; r++) {
 			for (var c = 0; c < this.bitmap[0].length; c++) {
 				if (this.bitmap[r][c] != this.empty) {
-					var square = this.table.rows[this.row + r].cells[this.col + c];
-					setClass(square, 'shape');
+					this.board.setShape(this.row + r, this.col + c);
 				}
 			}
 		}
@@ -169,9 +179,6 @@ Shape.prototype = {
 		this.bitmap = newBitmap;
 		this.show();
 	},
-	isEmpty: function(cell) {
-		return cell.className == 'empty';
-	},
 	blockedDown: function() {
 		// this.debugLog('blockedDown');
 		var row;
@@ -187,11 +194,11 @@ Shape.prototype = {
 				// this.debugLog('Column ' + col + ' is empty');
 				continue; // empty column
 			}
-			if (this.row + row + 1 >= this.table.rows.length) {
+			if (this.row + row + 1 >= this.board.getHeight()) {
 				// this.debugLog('Blocked by bottom of board');
 				return true; // blocked by bottom of board
 			}
-			if (!this.isEmpty(this.table.rows[this.row + row + 1].cells[this.col + col])) {
+			if (!this.board.isEmpty(this.row + row + 1, this.col + col)) {
 				// this.debugLog('Blocked by existing square at ' + this.row + ' + ' + row + ' + 1 = ' + (this.row + row + 1) + ', ' + this.col + ' + ' + col + ' = ' + (this.col + col));
 				return true; // blocked by existing square
 			}
@@ -214,11 +221,11 @@ Shape.prototype = {
 				// this.debugLog('Row ' + row + ' is empty');
 				continue; // empty row
 			}
-			if (this.col + col + 1 >= this.table.rows[this.row + row].cells.length) {
+			if (this.col + col + 1 >= this.board.getWidth()) {
 				// this.debugLog('Blocked by right of board');
 				return true; // blocked by right of board
 			}
-			if (!this.isEmpty(this.table.rows[this.row + row].cells[this.col + col + 1])) {
+			if (!this.board.isEmpty(this.row + row, this.col + col + 1)) {
 				// this.debugLog('Blocked by existing square at ' + this.row + ' + ' + row + ' = ' + (this.row + row) + ', ' + this.col + ' + ' + col + ' + 1 = ' + (this.col + col + 1));
 				return true; // blocked by existing square
 			}
@@ -245,7 +252,7 @@ Shape.prototype = {
 				// this.debugLog('Blocked by left of board');
 				return true; // blocked by left of board
 			}
-			if (!this.isEmpty(this.table.rows[this.row + row].cells[this.col + col - 1])) {
+			if (!this.board.isEmpty(this.row + row, this.col + col - 1)) {
 				// this.debugLog('Blocked by existing square at ' + this.row + ' + ' + row + ' = ' + (this.row + row) + ', ' + this.col + ' + ' + col + ' - 1 = ' + (this.col + col - 1));
 				return true; // blocked by existing square
 			}
@@ -255,6 +262,9 @@ Shape.prototype = {
 	},
 	fall: function() {
 		if (this.blockedDown()) {
+			// No longer the current shape - now just an obstacle
+			this.board.currentShape = null;
+			this.board.zapFilledRows();
 			return false;
 		}
 		this.hide();
@@ -286,3 +296,52 @@ Shape.prototype = {
 		}
 	}
 };
+
+shapeBitmaps[0] =
+	[
+	 'XX',
+	 'XX',
+	 ];
+
+shapeBitmaps[1] =
+	[
+	 '  X  ',
+	 '  X  ',
+	 '  X  ',
+	 '  X  '
+	];
+
+shapeBitmaps[2] =
+	[
+	 ' X',
+	 'XX',
+	 'X '
+	];
+
+shapeBitmaps[3] =
+	[
+	 'X ',
+	 'XX',
+	 ' X'
+	];
+
+shapeBitmaps[4] =
+	[
+	 ' X ',
+	 ' XX',
+	 ' X '
+	];
+
+shapeBitmaps[5] =
+	[
+	 ' X ',
+	 ' X ',
+	 ' XX'
+	];
+
+shapeBitmaps[6] =
+	[
+	 ' X ',
+	 ' X ',
+	 'XX '
+	];
